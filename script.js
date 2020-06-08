ad60b85a61ca63467ad0827eee57697e57f320c2
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2020-06-07 23:38:57 -0700

    randomly generating src values using the random numbers and dice.png layouts. Still has bug that random number generates 0, when that happens, there is no dice value, thus an image doesn't show

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-// Random Number 1
+// Random Number 1 & 2
 var randomNumber1 = Math.floor(Math.round(Math.random()*6));
-console.log(randomNumber1);
+var randomNumber2 = Math.floor(Math.round(Math.random()*6));
+console.log(randomNumber2);
+
+// Dice Image lable
+var dice1 = "dice" + randomNumber1 + ".png"
+var dice2 = "dice" + randomNumber2+ ".png"
+console.log(dice2);
+
+// retrieve dice image html tag
+var diceImage1 = document.querySelector("img.img1");
+var diceImage2 = document.querySelector("img.img2");
+console.log(diceImage2);
+
+// modify dice image tags with new dice image lable 
+diceImage1.setAttribute("src" , "./images/" + dice1);
+diceImage2.setAttribute("src" , "./images/" + dice2);
+console.log(diceImage2);
